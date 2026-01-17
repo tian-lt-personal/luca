449fc5c75155adfd308d5a67da8d394773e6fce5
tian <tian.lt@outlook.com>
2026-01-17 23:51:06 +0800

    .

--- src/LucaParserTests/ParserTests.cs
+++ src/LucaParserTests/ParserTests.cs
@@ -18,9 +18,35 @@ public class ParserTests
     [InlineData("(x y) z")]
     [InlineData("x (y z)")]
     [InlineData("(x (y z))")]
-    public void SimpleAbstractions(string source)
+    public void BasicExpr(string source)
     {
         var parser = new OneTimeParser(source);
         var tree = parser.RunPass();
+    }
+
+    [Theory]
+    [InlineData("(")]
+    [InlineData(")")]
+    [InlineData("(x")]
+    [InlineData("x)")]
+    [InlineData("x.")]
+    [InlineData(".x")]
+    [InlineData(".x.")]
+    [InlineData("(.x.")]
+    [InlineData(".x.)")]
+    [InlineData("x.y.")]
+    [InlineData(".x.y.")]
+    public void BadExpr(string source)
+    {
+        try
+        {
+            var parser = new OneTimeParser(source);
+            parser.RunPass();
+        }
+        catch (ParseError)
+        {
+            return;
+        }
+        Assert.Fail("Expected ParseError");
     }
 }
